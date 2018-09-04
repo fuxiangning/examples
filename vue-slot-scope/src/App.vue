d2cf448f272949e90abb8d9b273e655d42c077c8
秦少卫 <nihaojob@163.com>
2018-09-04 11:38:36 +0800

    update vue-slot Learn

---
+++ vue-slot-scope/src/App.vue
@@ -0,0 +1,96 @@
+<template>
+  <div id="app">
+   <h2>之前组件调用</h2>
+   <todo-list v-bind:todos="todosBefore" ></todo-list>
+    <hr>
+
+    <h2>之前组件调用</h2>
+    <todo-list v-bind:todos="todosAfter">
+      <template slot-scope="slotProps">
+        <div style="background:red; border-bottom:2px solid blue;">
+          {{slotProps.itemValue}}
+        </div>
+        <span v-if="slotProps.itemValue.isComplete">✓</span>
+        {{ slotProps.itemValue.test }}
+      </template>
+
+    </todo-list>
+
+  </div>
+</template>
+
+<script>
+import todoList from './components/todo-list.vue'
+
+export default {
+  name: 'app',
+  data:function(){
+    return {
+      todosBefore:[
+        {
+          test:'询问时间',
+          id:12312313123
+        },
+        {
+          test:'代办1',
+          id:123123123423423
+        },
+        {
+          test:'爱你地方年底见覅',
+          id:12312313123234234
+        },
+        {
+          test:'时间2',
+          id:1231231312323333
+        },
+        {
+          test:'师生情是行情',
+          id:12313333333
+        }
+      ],
+      todosAfter:[
+        {
+          test:'询问时间',
+          isComplete:true,
+          id:12312313123
+        },
+        {
+          test:'代办1',
+          isComplete:false,
+          id:123123123423423
+        },
+        {
+          test:'爱你地方年底见覅',
+          isComplete:false,
+          id:12312313123234234
+        },
+        {
+          test:'时间2',
+          isComplete:true,
+          id:1231231312323333
+        },
+        {
+          test:'师生情是行情',
+          isComplete:true,
+          id:12313333333
+        }
+      ],
+
+    }
+  },
+  components: {
+    todoList
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-slot-scope/src/components/todo-list.vue
@@ -0,0 +1,41 @@
+<template>
+  <ul>
+    <li v-for="item in todoList" v-bind:key="item.id">
+      <slot v-bind:itemValue = "item" >
+         {{ item.test }} 
+      </slot>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'todoList',
+  props: {
+    todos: Array
+  },
+  data(){
+    return {
+      todoList:this.todos
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display:block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
